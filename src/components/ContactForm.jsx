3548284fe5ab24b0e274b9bab82953aa82199240
notosarme <djinnstreasure@pm.me>
2024-04-19 12:33:36 -0400

    Contact form styling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,17 +1,16 @@
-import { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
-  const form = useRef();  
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const publicKey = process.env.EMAILJS_KEY;
 
   const sendEmail = (formData, e) => {
     e.preventDefault();
-
+    console.log(publicKey);
     emailjs
       .sendForm('service_np2hdth', 'contact_form', e.target, {
-        publicKey: '3DkRWHRxUSxEht_bQ',
+        publicKey: publicKey,
       })
       .then(
         () => {
@@ -24,25 +23,50 @@ const ContactForm = () => {
       );
   };
 
+  const labelStyle = {
+    display: 'block',
+    margin: '.25em 0'
+  }
+
+  const errorMessageStyle = {
+    color: 'red', 
+    marginLeft: '1rem',
+    fontSize: '.85em',
+  };
+
   return (
     <form onSubmit={handleSubmit(sendEmail)}>
-      <label>Name</label>
-      <input type="text" {...register('user_name', { required: true })} />
-      {errors.user_name && <span>This field is required</span>}
+      <div>
+        <label style={labelStyle}>Name 
+          {errors.user_name && <span style={errorMessageStyle}>This field is required</span>}
+        </label>
+        
+        <input placeholder="Name" type="text" label="name" {...register('user_name', { required: true })} />
+      </div>
+
+      <div>
+        <label style={labelStyle}>Email 
+          {errors.user_email && <span style={errorMessageStyle}>This field is required</span>}
+        </label>
+        <input type="email" placeholder='Email' label='Email' {...register('user_email', { required: true })} />
+      </div>
       
-      <label>Email</label>
-      <input type="email" {...register('user_email', { required: true })} />
-      {errors.user_email && <span>This field is required</span>}
+      <div>
+        <label style={labelStyle}>Subject 
+          {errors.subject && <span style={errorMessageStyle}>This field is required</span>}
+        </label>
+        
+        <input type="text" placeholder='Subject' label='Subject' {...register('subject', { required: true })} />
+      </div>
       
-      <label>Subject</label>
-      <input type="text" {...register('subject', { required: true })} />
-      {errors.subject && <span>This field is required</span>}
+      <div>
+        <label style={labelStyle}>Message 
+          {errors.message && <span style={errorMessageStyle}>This field is required</span>}
+        </label>
+        <textarea placeholder='Message' label='Message' {...register('message', { required: true })} />
+      </div>
       
-      <label>Message</label>
-      <textarea {...register('message', { required: true })} />
-      {errors.message && <span>This field is required</span>}
-      
-      <input type="submit" value="Send" />
+      <div><input type="submit" value="Send" id="input-button" /></div>
     </form>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,44 @@
 import ContactForm from "../components/ContactForm";
 
+const contentContainerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "space-between"
+};
+
+const leftStyle = {
+  flex: "1",
+  minWidth: "50%",
+  order: 1 // Initially, left will come first
+};
+
+const rightStyle = {
+  flex: "1",
+  minWidth: "50%",
+  order: 2 // Initially, right will come second
+};
+
 const Contact = () => {
   return (
     <div>
       <h2>Contact Us</h2>
-      <p>Get in touch with us.</p>
-      <ContactForm />
+      <div style={contentContainerStyle}>
+        <div className="left" style={leftStyle}><ContactDetails /></div>
+        <div className="right" style={rightStyle}><ContactForm /></div>
+      </div>
     </div>
+  );
+};
+
+const ContactDetails = () => {
+  return (
+    <>
+    <p>Feel free to leave a message via the form, or get a hold of me below.</p>
+    <p>Instagram | @xxxxxxx</p>
+    <p>Instagram | @xxxxxxx</p>
+    <p>Instagram | @xxxxxxx</p>
+    </>
   );
 };
 
