b7ec0a149d676d0bde5bfc895339ebcacbe4321b
notosarme <djinnstreasure@pm.me>
2024-04-22 18:11:34 -0400

    Update about page and css

--- src/components/AboutArtist.jsx
+++ src/components/AboutArtist.jsx
@@ -1,29 +1,17 @@
-const AboutArtist = () => {
+import "./AboutArtist.css";
 
-  const contentContainerStyle = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between"
-  };
-
-  const leftStyle = {
-    flex: "1",
-    minWidth: "50%",
-    order: 1 // Initially, left will come first
-  };
-
-  const rightStyle = {
-    flex: "1",
-    minWidth: "50%",
-    order: 2 // Initially, right will come second
-  };
-
+const AboutArtist = ({aboutText, imageUrl}) => {
   return (
-      <div style={contentContainerStyle}>
-        <div className="left" style={leftStyle}><p>Text Here</p></div>
-        <div className="right" style={rightStyle}><p>An image will go here</p></div>
+    <div className="aboutContainer">
+      <div className="left">
+        {aboutText.map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
       </div>
+      <div className="right">
+        <img src={imageUrl} alt="artist profile" loading="lazy" />
+      </div>
+    </div>
   );
 };
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,17 +2,31 @@ import AboutArtist from "../components/AboutArtist";
 import PDFViewer from "../components/PDFViewer";
 
 const About = () => {
+  //Each section in quotes, separated with a comma, will be a new paragraph in the Left section.
+  const aboutText = [
+    `Your custom paragraph text goes here.`,
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Donec ornare neque in nulla luctus, ut congue elit rutrum. 
+    Morbi vitae lectus eu ex bibendum dapibus et nec enim. 
+    Pellentesque quis diam pharetra, sodales leo a, malesuada dui. 
+    Integer gravida vitae tortor blandit pharetra. 
+    Ut malesuada mattis scelerisque. 
+    Aenean urna quam, vehicula id ultricies a, consequat at risus. 
+    Proin luctus finibus eros vitae congue. 
+    Mauris ac malesuada magna. Nam eu augue dolor. 
+    Curabitur ac est tempor enim blandit imperdiet sit amet vitae neque.`,
+    `And yet another one!`
+  ];
 
-  const aboutStyle = {
-    // display: "flex",
-    // flexDirection: "column",
-    backgroundColor: "lime"
-  };
-  
+  //This is the right profile image. It can be a hyperlink or a link to an asset.
+  const imageUrl = "https://picsum.photos/500/600"; 
+
+  //For the PDF, just replace assets/ARTIST_CV.pdf with the same name.
+
   return (
-    <div style={aboutStyle}>
+    <div>
       <h2>About Us</h2>
-      <AboutArtist />
+      <AboutArtist aboutText={aboutText} imageUrl={imageUrl} />
       <PDFViewer />
     </div>
   );
