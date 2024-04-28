b4383f2cdbfb0d7dc5296165d6fc79a0b55ba095
notosarme <djinnstreasure@pm.me>
2024-04-28 10:57:22 -0400

    Firebase api issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    define: {
+      'process.env': dotenv.config().parsed
+    },
     plugins: [react()],
     // base: "https://notosarme.github.io/deseronii",
     build: { chunkSizeWarningLimit: 1000, },
