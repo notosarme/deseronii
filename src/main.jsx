e1f93963fd8067ab488d3dfa3197f118c71364ec
notosarme <djinnstreasure@pm.me>
2024-04-28 12:23:31 -0400

    Fix vercel redirect error

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import { HashRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 
