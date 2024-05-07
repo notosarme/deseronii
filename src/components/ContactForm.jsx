4d3d3e634aedc9c95b212e7d65b794f4ee75f709
notosarme <djinnstreasure@pm.me>
2024-05-07 00:24:46 -0400

    Update labels for contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import { sendEmail } from '../utils/emailFunctions';
 
 const ContactForm = () => {
+  //TODO: Add submission alert
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
   const formRef = useRef();
   const emailKey = 'service_rjicbj5';
@@ -16,33 +17,33 @@ const ContactForm = () => {
   return (
     <form ref={formRef} onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label>Name 
+        <label htmlFor="name">Name 
           {errors.user_name && <span className="formErrorMessage">This field is required</span>}
         </label>
         
-        <input type="text" label="name" {...register('user_name', { required: true })} />
+        <input type="text" id="name" label="name" {...register('user_name', { required: true })} />
       </div>
 
       <div>
-        <label>Email 
+        <label htmlFor="email">Email 
           {errors.user_email && <span className="formErrorMessage">This field is required</span>}
         </label>
-        <input type="email" label='Email' {...register('user_email', { required: true })} />
+        <input type="email" id="email" label='Email' {...register('user_email', { required: true })} />
       </div>
       
       <div>
-        <label>Subject 
+        <label htmlFor="subject">Subject 
           {errors.subject && <span className="formErrorMessage">This field is required</span>}
         </label>
         
-        <input type="text" label='Subject' {...register('subject', { required: true })} />
+        <input type="text" id="subject" label='Subject' {...register('subject', { required: true })} />
       </div>
       
       <div>
-        <label>Message 
+        <label htmlFor="message">Message 
           {errors.message && <span className="formErrorMessage">This field is required</span>}
         </label>
-        <textarea label='Message' {...register('message', { required: true })} />
+        <textarea label='Message' id="message" {...register('message', { required: true })} />
       </div>
       
       <button className='button' type="submit">
